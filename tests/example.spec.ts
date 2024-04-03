ecd142d870fd9f1092c4f94f609ce93c39aa88af
alejandro <alejandrop@bluetrailsoft.com>
2024-04-03 14:12:15 -0600

    [Docker-compose]: index.html added, docker-compose added and playwright tests updated

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,7 @@ test('simple test', async ({ page }) => {
   await page.goto('https://demoqa.com/login');
 
   let usernameValue: string = "DogDeveloper1";
-  let passwordValue: string = "_Dog@18234";
-  let passwordValues: string = "_Dog@18234";
+  let passwordValue: string = "_Dog@18234"; 
 
   await page.locator('xpath=//*[@id="userName"]').type(usernameValue);
   await page.locator('xpath=//*[@id="password"]').type(passwordValue);
